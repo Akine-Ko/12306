24ae02595f9926ef38e15778a06724bacaa4a6be
Akine <2923601304@qq.com>
2025-07-16 18:03:41 +0800

    4.8增加公共模块common,用于放置公共的类，比如工具类，拦截器，AOP，常量，枚举，公共配置等

---
+++ member/src/main/java/com/akine/mytrain/member/config/MemberApplication.java
@@ -0,0 +1,23 @@
+package com.akine.mytrain.member.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.akine")
+public class MemberApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(MemberApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功！");
+        log.info("地址:\thttp://127.0.0.1:{}/{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+
+    }
+}
