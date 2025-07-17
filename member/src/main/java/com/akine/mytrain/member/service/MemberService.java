50dc4117a3a99b48f40eed780de9a1e5b0a2ff2a
Akine <2923601304@qq.com>
2025-07-17 19:41:20 +0800

    4.15实现一个简单的注册接口

--- member/src/main/java/com/akine/mytrain/member/service/MemberService.java
+++ member/src/main/java/com/akine/mytrain/member/service/MemberService.java
@@ -1,9 +1,13 @@
 package com.akine.mytrain.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.akine.mytrain.member.domain.Member;
 import com.akine.mytrain.member.domain.MemberExample;
 import com.akine.mytrain.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -13,5 +17,23 @@ public class MemberService {
 
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(new MemberExample()));
+    }
+
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        if(CollUtil.isNotEmpty(list)){
+            //return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+
+        return member.getId();
     }
 }
