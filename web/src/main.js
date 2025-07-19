12bf1cdf1c9e807ac35fb05b75af13e23c8541dc
Akine <2923601304@qq.com>
2025-07-19 15:23:11 +0800

    5.11 Vue CLI多环境配置：为axios配置后端域名

--- web/src/main.js
+++ web/src/main.js
@@ -17,17 +17,21 @@ for(const i in icons) {
 
 // Axios拦截器
 axios.interceptors.request.use(function (config) {
-    console.log("请求参数: ", config);
+    console.log('请求参数: ', config);
     return config;
 }, error => {
     return Promise.reject(error);
 });
 
 axios.interceptors.response.use(function (response) {
-    console.log("返回结果: ", response);
+    console.log('返回结果: ', response);
     return response;
 },error => {
-    console.log("返回错误:",error);
+    console.log('返回错误: ',error);
     return Promise.reject(error);
 });
+
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+console.log('环境: ', process.env.NODE_ENV);
+console.log('服务端: ', process.env.VUE_APP_SERVER);
 
