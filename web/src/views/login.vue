5d9943ddece72baea65b1aa18110649648eae89f
Akine <2923601304@qq.com>
2025-07-20 11:08:24 +0800

    6.5 使用vuex(store)来保存登录信息

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -42,6 +42,7 @@ import { defineComponent, reactive } from 'vue';
 import axios from 'axios';
 import { notification } from 'ant-design-vue';
 import { useRouter } from 'vue-router';
+import store from "@/store";
 
 
 export default defineComponent({
@@ -78,6 +79,7 @@ export default defineComponent({
           notification.success({ description: '登录成功！' });
           // 登录成功，调到控台主页
           router.push({ path: '/' });
+          store.commit("setMember", data.content)
         } else {
           notification.error({ description: data.message });
         }
