9351de6fd914ae629f3134c2dd0b3c98708d931b
Akine <2923601304@qq.com>
2025-07-20 09:27:39 +0800

    5.13 vue组件开发，增加the-header组件

---
+++ web/src/components/the-header.vue
@@ -0,0 +1,45 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 1111</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+import {defineComponent, ref} from 'vue';
+
+
+export default defineComponent({
+  name: "the-header-view",
+  setup() {
+
+    return {
+      selectedKeys1: ref(['2']),
+    };
+  },
+});
+</script>
+
+<style scoped>
+.login-main h1 {
+  font-size: 25px;
+  font-weight: bold;
+}
+.login-main {
+  margin-top: 100px;
+  padding: 30px 30px 20px;
+  border: 2px solid grey;
+  border-radius: 10px;
+  background-color: #fcfcfc;
+}
+
+</style>
