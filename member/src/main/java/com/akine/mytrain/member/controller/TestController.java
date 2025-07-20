d7b6be1c938398eeb9866936c7aa558eb571f826
Akine <2923601304@qq.com>
2025-07-20 19:38:52 +0800

    6.7演示gateway过滤器的使用

---
+++ member/src/main/java/com/akine/mytrain/member/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.akine.mytrain.member.controller;
+
+import com.akine.mytrain.common.resp.CommonResp;
+import com.akine.mytrain.member.service.MemberService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class TestController {
+
+    @Resource
+    private MemberService memberService;
+
+    @GetMapping("/hello")
+    public CommonResp<String> hello(){
+        CommonResp<String> commonResp = new CommonResp<>();
+        commonResp.setContent("hello world");
+        return commonResp;
+    }
+}
