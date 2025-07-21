5765cbb5480bbf6efb88caf097bebd4b57180cf2
Akine <2923601304@qq.com>
2025-07-21 11:30:35 +0800

    7.3 增加乘车人新增接口

---
+++ member/src/main/java/com/akine/mytrain/member/controller/PassengerController.java
@@ -0,0 +1,26 @@
+package com.akine.mytrain.member.controller;
+
+import com.akine.mytrain.common.resp.CommonResp;
+import com.akine.mytrain.member.req.PassengerSaveReq;
+import com.akine.mytrain.member.service.PassengerService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/passenger")
+public class PassengerController {
+
+    @Resource
+    private PassengerService passengerService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req) {
+        passengerService.save(req);
+        return new CommonResp<>();
+    }
+
+}
