e6a070bdc3e7c02d076c04f88a3569a1c2ec4965
Akine <2923601304@qq.com>
2025-07-21 10:10:42 +0800

    6.10 增加路由登录拦截，访问所有的控台页面都需要登录

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from "@/store";
 
 const routes = [
   {
@@ -7,7 +8,10 @@ const routes = [
   },
   {
     path: '/',
-    component: () => import('../views/main.vue')
+    component: () => import('../views/main.vue'),
+    meta:{
+      loginRequire:true
+    }
   }
 ]
 
@@ -15,5 +19,24 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(function (item){
+    console.log(item, "是否需要登录校验", item.meta.loginRequire || false);
+    return item.meta.loginRequire
+  })) {
+    const _member = store.state.member
+    console.log("页面登录校验开始:", _member);
+
+    if (!_member.token) {
+      console.log("用户未登录或登录超时!");
+      next('/login');
+    } else {
+      next();
+    }
+  }else{
+      next();
+    }
+});
 
 export default router
