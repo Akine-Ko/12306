1aa229ed259c89aae461f818c9f725cbdac00a52
Akine <2923601304@qq.com>
2025-07-22 10:31:26 +0800

    7.5 增加日志拦截器，统一打印日志流水号

--- member/src/main/java/com/akine/mytrain/member/config/SpringMvcConfig.java
+++ member/src/main/java/com/akine/mytrain/member/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.akine.mytrain.member.config;
 
+import com.akine.mytrain.common.interceptor.LogInterceptor;
 import com.akine.mytrain.common.interceptor.MemberInterceptor;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SpringMvcConfig implements WebMvcConfigurer {
 
     @Resource
+    LogInterceptor logInterceptor;
+
+    @Resource
     MemberInterceptor memberInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
         registry.addInterceptor(memberInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/member/member/send-code",
