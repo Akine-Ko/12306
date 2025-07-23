c0a4c54cc2d42ac7dfd4a6e85cd9311af9bc91dd
Akine <2923601304@qq.com>
2025-07-23 16:40:53 +0800

    7.18 将枚举数组放进单独的js文件

--- web/src/main.js
+++ web/src/main.js
@@ -4,8 +4,9 @@ import router from './router'
 import store from './store'
 import Antd, {notification} from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-import * as Icons from '@ant-design/icons-vue'
-import axios from 'axios'
+import * as Icons from '@ant-design/icons-vue';
+import axios from 'axios';
+import './assets/js/enums';
 
 const app = createApp(App);
 app.use(Antd).use(store).use(router).mount('#app');
