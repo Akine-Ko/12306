c8b84fa680178ad17b453eb1a6c5adcf1210e45c
Akine <2923601304@qq.com>
2025-07-25 15:37:50 +0800

    admin去掉登录页面，去掉member和登录相关的拦截，去掉乘车人页面

---
+++ admin/src/views/main/about.vue
@@ -0,0 +1,19 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {
+
+    };
+  },
+});
+</script>
+
+<template>
+  <h1>关于123306</h1>
+</template>
+
+<style scoped>
+
+</style>
