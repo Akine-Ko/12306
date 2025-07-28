f3df5e97ce93a8e8eb4dd79ff41642a14dfc447d
Akine <2923601304@qq.com>
2025-07-28 19:50:12 +0800

    增加quartz相关的12张表，增加接口来操作quartz

---
+++ batch/src/main/java/com/akine/mytrain/batch/config/SpringMvcConfig.java
@@ -0,0 +1,24 @@
+package com.akine.mytrain.batch.config;
+
+import com.akine.mytrain.common.interceptor.LogInterceptor;
+import com.akine.mytrain.common.interceptor.MemberInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    @Resource
+    MemberInterceptor memberInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**");
+    }
+}
