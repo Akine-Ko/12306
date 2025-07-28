5e7452f68fee1caa622e377106c039a299811afd
Akine <2923601304@qq.com>
2025-07-28 16:19:28 +0800

    尝试禁止任务并发执行

--- batch/src/main/java/com/akine/mytrain/batch/job/TestJob.java
+++ batch/src/main/java/com/akine/mytrain/batch/job/TestJob.java
@@ -1,14 +1,25 @@
 package com.akine.mytrain.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST");
+        System.out.println("TestJob TEST 开始");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("TestJob TEST 结束");
     }
 }
