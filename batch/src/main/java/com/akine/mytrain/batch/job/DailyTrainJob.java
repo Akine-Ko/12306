134ec56ef6c9b08b7d25c96552ad04e3ee8d10fc
Akine <2923601304@qq.com>
2025-07-29 20:22:37 +0800

    增加生成每日车次功能

--- batch/src/main/java/com/akine/mytrain/batch/job/DailyTrainJob.java
+++ batch/src/main/java/com/akine/mytrain/batch/job/DailyTrainJob.java
@@ -1,6 +1,11 @@
 package com.akine.mytrain.batch.job;
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.RandomUtil;
+import com.akine.mytrain.batch.feign.BusinessFeign;
+import com.akine.mytrain.common.resp.CommonResp;
+import jakarta.annotation.Resource;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -9,18 +14,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import java.util.Date;
+
 
 @DisallowConcurrentExecution
 public class DailyTrainJob implements Job {
 
     private static final Logger logger = LoggerFactory.getLogger(DailyTrainJob.class);
 
+    @Resource
+    BusinessFeign businessFeign;
+
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         //增加日志流水号
         MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
 
-        logger.info("生成每日车次数据开始");
-        logger.info("生成每日车次数据结束");
+        logger.info("生成15天后的车次数据开始");
+
+        Date date = new Date();
+        DateTime dateTime = DateUtil.offsetDay(date, 15);
+        Date offsetDate = dateTime.toJdkDate();
+        CommonResp<Object> commonResp = businessFeign.genDaily(offsetDate);
+
+        logger.info("生成15天后的车次数据结束, 结果{}", commonResp);
     }
 }
