d7b49ef1a4959766b878e2e9baf453860aebe695
Akine <2923601304@qq.com>
2025-07-29 19:37:11 +0800

    集成openfeign，实现服务间调用

--- batch/src/main/java/com/akine/mytrain/batch/controller/TestController.java
+++ batch/src/main/java/com/akine/mytrain/batch/controller/TestController.java
@@ -1,13 +1,24 @@
 package com.akine.mytrain.batch.controller;
 
+import com.akine.mytrain.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Resource
+    BusinessFeign businessFeign;
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("/hello")
     public String hello(){
+        String hello = businessFeign.hello();
+        logger.info(hello);
         return "hello world, batch!";
     }
 }
