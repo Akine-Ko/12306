2daabb624229e5e2011234eb26d7766bbe3dba32
Akine <2923601304@qq.com>
2025-07-30 11:33:32 +0800

    增加余票信息表，生成前后端代码

---
+++ business/src/main/java/com/akine/mytrain/business/req/DailyTrainTicketQueryReq.java
@@ -0,0 +1,12 @@
+package com.akine.mytrain.business.req;
+
+import com.akine.mytrain.common.req.PageReq;
+
+public class DailyTrainTicketQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainTicketQueryReq{" +
+                "} " + super.toString();
+    }
+}
