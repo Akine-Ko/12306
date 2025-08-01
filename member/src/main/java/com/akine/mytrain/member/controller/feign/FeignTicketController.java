f3520353064a36b683af0f7c7089d9630c57c303
Akine <2923601304@qq.com>
2025-08-01 20:27:28 +0800

    增加一个feign接口，保存车票信息

---
+++ member/src/main/java/com/akine/mytrain/member/controller/feign/FeignTicketController.java
@@ -0,0 +1,26 @@
+package com.akine.mytrain.member.controller.feign;
+
+import com.akine.mytrain.common.resp.CommonResp;
+import com.akine.mytrain.member.req.MemberTicketReq;
+import com.akine.mytrain.member.service.TicketService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/feign/ticket")
+public class FeignTicketController {
+
+    @Resource
+    private TicketService ticketService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody MemberTicketReq req) throws Exception {
+        ticketService.save(req);
+        return new CommonResp<>();
+    }
+
+}
--- member/src/main/java/com/akine/mytrain/member/service/TicketService.java
+++ member/src/main/java/com/akine/mytrain/member/service/TicketService.java
@@ -2,14 +2,13 @@ package com.akine.mytrain.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
-import cn.hutool.core.util.ObjectUtil;
 import com.akine.mytrain.common.resp.PageResp;
 import com.akine.mytrain.common.util.SnowUtil;
 import com.akine.mytrain.member.domain.Ticket;
 import com.akine.mytrain.member.domain.TicketExample;
 import com.akine.mytrain.member.mapper.TicketMapper;
+import com.akine.mytrain.member.req.MemberTicketReq;
 import com.akine.mytrain.member.req.TicketQueryReq;
-import com.akine.mytrain.member.req.TicketSaveReq;
 import com.akine.mytrain.member.resp.TicketQueryResp;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -28,20 +27,18 @@ public class TicketService {
     @Resource
     private TicketMapper ticketMapper;
 
-    public void save(TicketSaveReq req) {
+    /**
+     * 会员购买车票后新增保存
+     *
+     * @param req
+     */
+    public void save(MemberTicketReq req) throws Exception {
         DateTime now = DateTime.now();
         Ticket ticket = BeanUtil.copyProperties(req, Ticket.class);
-        if(ObjectUtil.isNull(ticket.getId())){
-            ticket.setId(SnowUtil.getSnowflakeNextId());
-            ticket.setCreateTime(now);
-            ticket.setUpdateTime(now);
-            ticketMapper.insert(ticket);
-        }
-        else{
-            ticket.setUpdateTime(now);
-            ticketMapper.updateByPrimaryKey(ticket);
-        }
-
+        ticket.setId(SnowUtil.getSnowflakeNextId());
+        ticket.setCreateTime(now);
+        ticket.setUpdateTime(now);
+        ticketMapper.insert(ticket);
     }
 
     public PageResp<TicketQueryResp> queryList(TicketQueryReq req) {
