5f916f16b144608b5fbbd9a512f7bfc416526476
Akine <2923601304@qq.com>
2025-08-02 20:51:26 +0800

    研究mybatis的一级缓存

--- business/src/main/java/com/akine/mytrain/business/service/TrainService.java
+++ business/src/main/java/com/akine/mytrain/business/service/TrainService.java
@@ -20,13 +20,14 @@ import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class TrainService {
 
-    private static Logger logger = LoggerFactory.getLogger(TrainService.class);
+    private static final Logger logger = LoggerFactory.getLogger(TrainService.class);
 
     @Resource
     private TrainMapper trainMapper;
@@ -91,8 +92,11 @@ public class TrainService {
     }
 
 
+    @Transactional
     public List<TrainQueryResp> queryAll() {
         List<Train> trainList = selectAll();
+//        logger.info("再查一次");
+//        trainList = selectAll();
         return BeanUtil.copyToList(trainList, TrainQueryResp.class);
     }
 
