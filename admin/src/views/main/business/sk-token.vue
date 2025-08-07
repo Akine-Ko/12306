91fad78be67d20c09f1ebde27b8861dc61bf7834
Akine <2923601304@qq.com>
2025-08-07 09:41:04 +0800

    生成后端文件

---
+++ admin/src/views/main/business/sk-token.vue
@@ -0,0 +1,118 @@
+<template>
+  <p>
+    <a-space>
+      <a-button type="primary" @click="handleQuery()">刷新</a-button>
+      
+    </a-space>
+  </p>
+  <a-table :dataSource="skTokens"
+           :columns="columns"
+           :pagination="pagination"
+           @change="handleTableChange"
+           :loading="loading">
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.dataIndex === 'operation'">
+      </template>
+    </template>
+  </a-table>
+</template>
+
+<script>
+import { defineComponent, ref, onMounted } from 'vue';
+import {notification} from "ant-design-vue";
+import axios from "axios";
+
+export default defineComponent({
+  name: "sk-token-view",
+  setup() {
+    const visible = ref(false);
+    let skToken = ref({
+      id: undefined,
+      date: undefined,
+      trainCode: undefined,
+      count: undefined,
+      createTime: undefined,
+      updateTime: undefined,
+    });
+    const skTokens = ref([]);
+    // 分页的三个属性名是固定的
+    const pagination = ref({
+      total: 0,
+      current: 1,
+      pageSize: 10,
+    });
+    let loading = ref(false);
+    const columns = [
+    {
+      title: '日期',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: '车次编号',
+      dataIndex: 'trainCode',
+      key: 'trainCode',
+    },
+    {
+      title: '令牌余量',
+      dataIndex: 'count',
+      key: 'count',
+    },
+    ];
+
+
+    const handleQuery = (param) => {
+      if (!param) {
+        param = {
+          page: 1,
+          size: pagination.value.pageSize
+        };
+      }
+      loading.value = true;
+      axios.get("/business/admin/sk-token/query-list", {
+        params: {
+          page: param.page,
+          size: param.size
+        }
+      }).then((response) => {
+        loading.value = false;
+        let data = response.data;
+        if (data.success) {
+          skTokens.value = data.content.list;
+          // 设置分页控件的值
+          pagination.value.current = param.page;
+          pagination.value.total = data.content.total;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    const handleTableChange = (page) => {
+      pagination.value.pageSize = page.pageSize;
+      handleQuery({
+        page: page.current,
+        size: page.pageSize
+      });
+    };
+
+    onMounted(() => {
+      handleQuery({
+        page: 1,
+        size: pagination.value.pageSize
+      });
+    });
+
+    return {
+      skToken,
+      visible,
+      skTokens,
+      pagination,
+      columns,
+      handleTableChange,
+      handleQuery,
+      loading,
+    };
+  },
+});
+</script>
