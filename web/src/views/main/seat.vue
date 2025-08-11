fc59f25bcb923a3e56b305be46c651ba1a4763b1
Akine <2923601304@qq.com>
2025-08-11 18:34:09 +0800

    座位销售图页面获得销售信息，同一趟车，不管查哪个区间，查到的销售信息是一样的，由界面再去截取区间的销售信息。

--- web/src/views/main/seat.vue
+++ web/src/views/main/seat.vue
@@ -9,13 +9,18 @@
     <p>
       日期：{{param.date}}，车次：{{param.trainCode}}，出发站：{{param.start}}，到达站：{{param.end}}
     </p>
+    <p>
+      {{list}}
+    </p>
   </div>
 </template>
 
 <script>
 
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import axios from "axios";
 import {useRoute} from "vue-router";
+import {notification} from "ant-design-vue";
 
 export default defineComponent({
   name: "welcome-view",
@@ -23,9 +28,35 @@ export default defineComponent({
     const route = useRoute();
     const param = ref({});
     param.value = route.query;
+    const list = ref();
+
+    // 查询一列火车的所有销售信息
+    const querySeat = () => {
+      axios.get("/business/seat-sell/query", {
+        params: {
+          date: param.value.date,
+          trainCode: param.value.trainCode,
+        }
+      }).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          list.value = data.content;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    onMounted(() => {
+      if (param.value.date) {
+        querySeat();
+      }
+    });
 
     return {
-      param
+      param,
+      list,
+      querySeat,
     };
   },
 });
