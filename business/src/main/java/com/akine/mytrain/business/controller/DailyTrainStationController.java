df67c2b4c44f0497bf9b8f5c1d836d0dc9fa2659
Akine <2923601304@qq.com>
2025-08-11 16:02:43 +0800

    余票查询页面增加显示车站信息

---
+++ business/src/main/java/com/akine/mytrain/business/controller/DailyTrainStationController.java
@@ -0,0 +1,27 @@
+package com.akine.mytrain.business.controller;
+
+import com.akine.mytrain.business.resp.DailyTrainStationQueryResp;
+import com.akine.mytrain.business.service.DailyTrainStationService;
+import com.akine.mytrain.common.resp.CommonResp;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/daily-train-station")
+public class DailyTrainStationController {
+
+    @Autowired
+    private DailyTrainStationService dailyTrainStationService;
+
+    @GetMapping("/query-by-train-code")
+    public CommonResp<List<DailyTrainStationQueryResp>> queryByTrain(@Valid DailyTrainStationQueryAllReq req) {
+        List<DailyTrainStationQueryResp> list = dailyTrainStationService.queryByTrain(req.getDate(), req.getTrainCode());
+        return new CommonResp<>(list);
+    }
+
+}
